7fab8dfdbc531d09941a9b264a515c6776f4d9c7
kyuminMoon <kyumin.moon@togle.shop>
2022-11-27 22:46:29 +0900

    인프런 동시성 이슈 - 재고시스템 https://www.inflearn.com/course/%EB%8F%99%EC%8B%9C%EC%84%B1%EC%9D%B4%EC%8A%88-%EC%9E%AC%EA%B3%A0%EC%8B%9C%EC%8A%A4%ED%85%9C/dashboard

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,37 @@
+package com.example.stock.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Stock {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private Long productId;
+  private Long quantity;
+
+  @Version
+  private Long version;
+
+  public Stock(Long productId, Long quantity) {
+    this.productId = productId;
+    this.quantity = quantity;
+  }
+
+  public void decrease(Long quantity){
+    if(this.quantity - quantity < 0) {
+      throw new RuntimeException("foo");
+    }
+    this.quantity = this.quantity - quantity;
+  }
+}
